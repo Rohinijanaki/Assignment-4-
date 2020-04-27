3c2b66761e2c560845354f8393e96e7a97dddeff
rohini janaki <63153807+Rohinijanaki@users.noreply.github.com>
2020-04-27 17:16:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+(function() {
+var names=["yaakov","john","jen","Jason","paul","frank","larry","paula","laura","jim"];
+
+for (var i=0;i<names.length;i++){
+	var firstletter=names[i].charAt(0).toLowerCase();
+		if (firstletter==="j") {
+			helloSpeaker.speak(names[i]);
+	 	} 
+	    else {
+	   		byeSpeaker.speak(names[i]);
+	  	}
+	}
+})();
+
+
+
